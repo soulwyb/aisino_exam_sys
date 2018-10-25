1bdf40c05e3a7029289bad168fedac762350ab46
aisino <340145294@qq.com>
2018-10-25 18:05:59 +0800

    完成了题库管理页面再点击新建的时候能够带上相应组织的名字的JS代码。并且优化了遮罩层，接下来就是写弹出层的界面。

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,15 @@
 $(function(){
-    $('#create_department').focus(function(){
-        var height = $(document.body).outerWidth(true);
+    var flag = 0;
+    $('#create_department').click(function(){
+        var height = $(window).outerWidth(true);
+        var title = $('[id="custom-active"]').children('a').text();
         $('#custom-layer-display').css({"display":"block","height":height});
         $('#custom-float-display').css("display","block");
+        $('#custom-pop-box-title').text("题库添加 - " + title);
+        flag=1;
+    });
+    $('.custom-tree-selected').click(function(){
+        $("#custom-tree-js").find(".custom-tree-selected").removeAttr('id');
+        $(this).attr("id","custom-active");
     });
 })
